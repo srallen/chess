ebcbceea1a3407cb045cfbc5651b89cd4d7e8133
Sarah Allen <sarah@zooniverse.org>
2018-03-26 09:33:41 -0500

    Some clean up of unused files. Use class declaration for ChessBoard

--- src/components/ChessBoard/ChessBoard.js
+++ src/components/ChessBoard/ChessBoard.js
@@ -1,55 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import isEqual from 'lodash.isequal';
 import BoardSquare from './components/BoardSquare';
 import './ChessBoard.css';
 
-function determineWhichPiece(pieces, boardCoordinates) {
-  let whichPiece = [];
-  pieces.forEach((piece, index) => {
-    const pieceCoordinates = piece[1];
-    if (isEqual(pieceCoordinates, boardCoordinates)) {
-      whichPiece = piece;
-    }
-  });
+class ChessBoard extends Component {
+  determineWhichPiece(pieces, boardCoordinates) {
+    let whichPiece = [];
+    pieces.forEach((piece, index) => {
+      const pieceCoordinates = piece[1];
+      if (isEqual(pieceCoordinates, boardCoordinates)) {
+        whichPiece = piece;
+      }
+    });
 
-  return whichPiece;
-};
+    return whichPiece;
+  }
 
-export default function ChessBoard({ isMoveValid, onChange, onClick, pieces, selectedPiece }) {
-  const arrayOfEight = Array.from(Array(8));
-  return (
-    <div className="ChessBoard">
-      {arrayOfEight.map((element, index) => {
-        const yCoordinate = index + 1;
-        return (
-          <div key={yCoordinate} className="ChessBoard__row">
-            {arrayOfEight.map((element, index) => {
-              const xCoordinate = index + 1;
-              const boardCoordinates = [xCoordinate, yCoordinate];
-              const whichPiece = determineWhichPiece(pieces, boardCoordinates);
-              const checked = isEqual(selectedPiece, whichPiece);
-              const disabled = (whichPiece.length === 0 && selectedPiece === null) ||
-                (selectedPiece && !isMoveValid(boardCoordinates));
-              
-              return (
-                <BoardSquare
-                  boardCoordinates={boardCoordinates}
-                  checked={checked}
-                  disabled={disabled}
-                  invalid={(selectedPiece && !isMoveValid(boardCoordinates))}
-                  key={boardCoordinates}
-                  label={boardCoordinates.toString()}
-                  onChange={onChange}
-                  onClick={onClick}
-                  piece={whichPiece}
-                />
-              )
-            })}
-          </div>)}
-      )}
-    </div>
-  );
+  render() {
+    const { isMoveValid, onChange, onClick, pieces, selectedPiece } = this.props;
+    const arrayOfEight = Array.from(Array(8));
+    return (
+      <div className="ChessBoard">
+        {arrayOfEight.map((element, index) => {
+          const yCoordinate = index + 1;
+          return (
+            <div key={yCoordinate} className="ChessBoard__row">
+              {arrayOfEight.map((element, index) => {
+                const xCoordinate = index + 1;
+                const boardCoordinates = [xCoordinate, yCoordinate];
+                const whichPiece = this.determineWhichPiece(pieces, boardCoordinates);
+                const checked = isEqual(selectedPiece, whichPiece);
+                const disabled = (whichPiece.length === 0 && selectedPiece === null) ||
+                  (selectedPiece && !isMoveValid(boardCoordinates));
+                
+                return (
+                  <BoardSquare
+                    boardCoordinates={boardCoordinates}
+                    checked={checked}
+                    disabled={disabled}
+                    invalid={(selectedPiece && !isMoveValid(boardCoordinates))}
+                    key={boardCoordinates}
+                    label={boardCoordinates.toString()}
+                    onChange={onChange}
+                    onClick={onClick}
+                    piece={whichPiece}
+                  />
+                )
+              })}
+            </div>)}
+        )}
+      </div>
+    );
+  }
 }
 
 ChessBoard.propTypes = {
@@ -59,4 +62,6 @@ ChessBoard.propTypes = {
   pieces: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.number)]))),
   selectedPiece: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.number, PropTypes.number)]))
 };
+
+export default ChessBoard;
 
